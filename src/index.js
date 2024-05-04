818fd9d339344a867b5df96cdce924151bf4c1ee
espisepi <sepinaco@gmail.com>
2024-05-04 22:48:06 +0200

    gallery index to typescript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './styles.css' // OPCIONAL
 import {createGalleryWebcomponent} from './webcomponents/gallery';
 
+
+// Microfrontend hecho a partir del tutorial:
+// https://tekinico.medium.com/build-a-react-embeddable-widget-c46b7f7999d8
+// https://github.com/nicoraynaud/react-widget
+
 createGalleryWebcomponent();
--- src/webcomponents/gallery/index.tsx
+++ src/webcomponents/gallery/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-// import './styles.css'
 import { App } from './App'
 
 
@@ -12,32 +11,34 @@ export const createGalleryWebcomponent = () => {
   const WEBCOMPONENT_QUERY_SELECTOR_NAME = 'sepinaco-gallery-webcomponent';
 
   // Find all widget divs
-  const widgetDivs = document.querySelectorAll(`.${WEBCOMPONENT_QUERY_SELECTOR_NAME}, #${WEBCOMPONENT_QUERY_SELECTOR_NAME}`);
+  const widgetDivs:NodeListOf<HTMLElement> = document.querySelectorAll(`.${WEBCOMPONENT_QUERY_SELECTOR_NAME}, #${WEBCOMPONENT_QUERY_SELECTOR_NAME}`);
   
   // Inject our React App into each class
   widgetDivs.forEach(div => {
     const root = ReactDOM.createRoot(div);
-  
-    // get data-items attribute ==================
-    const itemsString = div?.dataset?.items;
-    const items = JSON.parse(itemsString);
-    // Convert String "[0,0,0]" => Array [0,0,0]
-    items.forEach(item => {
-      item.position = JSON.parse(item.position);
-      item.rotation = JSON.parse(item.rotation);
-    });
-  
+
     // get data-debug attribute ==================
     const debugString = div?.dataset?.debug;
     const debug = debugString === "true";
     console.log(WEBCOMPONENT_QUERY_SELECTOR_NAME + " Debug mode: ", debug);
   
-    // Show logs if debug is true
-    if(debug) {
-      console.log(itemsString);
-      console.log(items);
+    // get data-items attribute ==================
+    const itemsString: string | undefined = div?.dataset?.items;
+    let items = [];
+    if(itemsString) {
+      items = JSON.parse(itemsString);
+      // Convert String "[0,0,0]" => Array [0,0,0]
+      items.forEach((item: { position: string; rotation: string; }) => {
+        item.position = JSON.parse(item.position);
+        item.rotation = JSON.parse(item.rotation);
+      });
+      // Show logs if debug is true
+      if(debug) {
+        console.log({itemsString});
+        console.log({items});
+      }
     }
-  
+
     root.render(
       <React.StrictMode>
         <App items={items} />
